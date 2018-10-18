0c06c87b7a3d7b284cc6b115dc1ae966fee025d0
Abbass MAROUNI <amarouni@talend.com>
2018-10-18 16:14:12 +0200

    review fixes

--- processing-beam/src/test/java/org/talend/components/processing/flatten/FlattenUtilsTest.java
+++ processing-beam/src/test/java/org/talend/components/processing/flatten/FlattenUtilsTest.java
@@ -1,15 +1,3 @@
-// ============================================================================
-//
-// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
 package org.talend.components.processing.flatten;
 
 import org.apache.avro.Schema;
@@ -24,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FalttenUtilsTest {
+public class FlattenUtilsTest {
 
     private final Schema inputSchemaL = SchemaBuilder.record("inputRowL") //
             .fields() //
