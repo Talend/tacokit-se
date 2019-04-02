e1a138bbeed5152a75faf9f902690f8a48b9f8ff
pyzhou-talend <pyzhou@talend.com>
2019-04-02 11:51:32 +0800

    feat(TDI-41616):first commit

--- jdbc/src/main/java/org/talend/components/jdbc/output/platforms/MSSQLPlatform.java
+++ jdbc/src/main/java/org/talend/components/jdbc/output/platforms/MSSQLPlatform.java
@@ -28,7 +28,7 @@ public class MSSQLPlatform extends Platform {
 
     public static final String MSSQL = "mssql";
 
-    public static final String SQLDWH = "SQLDWH";
+    public static final String SQLDWH = "sqldwh";
 
     public MSSQLPlatform(final I18nMessage i18n) {
         super(i18n);
