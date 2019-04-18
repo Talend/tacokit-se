334653938702aac4e0195c71d5bdd63cc2b4aa49
pyzhou-talend <pyzhou@talend.com>
2019-04-18 11:37:36 +0800

    feat(TDI-41616): override create table

--- jdbc/src/main/java/org/talend/components/jdbc/output/platforms/SQLDWHPlatform.java
+++ jdbc/src/main/java/org/talend/components/jdbc/output/platforms/SQLDWHPlatform.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class SQLDWHPlatform extends MSSQLPlatform {
@@ -37,6 +38,64 @@ public class SQLDWHPlatform extends MSSQLPlatform {
             }
 
             log.trace("create table issue was ignored. The table and it's name space has been created by an other worker", e);
+        }
+    }
+
+    @Override
+    protected String buildQuery(final Table table) {
+        // keep the string builder for readability
+        final StringBuilder sql = new StringBuilder("CREATE TABLE");
+        sql.append(" ");
+        if (table.getSchema() != null && !table.getSchema().isEmpty()) {
+            sql.append(identifier(table.getSchema())).append(".");
+        }
+        sql.append(identifier(table.getName()));
+        sql.append("(");
+        sql.append(createColumns(table.getColumns()));
+        sql.append(createPKs(table.getName(),
+                table.getColumns().stream().filter(Column::isPrimaryKey).collect(Collectors.toList())));
+        sql.append(")");
+        // todo create index
+
+        log.debug("### create table query ###");
+        log.debug(sql.toString());
+        return sql.toString();
+    }
+
+    private String createColumns(final List<Column> columns) {
+        return columns.stream().map(this::createColumn).collect(Collectors.joining(","));
+    }
+
+    private String createColumn(final Column column) {
+        return identifier(column.getName())//
+                + " " + toDBType(column)//
+                + " " + isRequired(column)//
+        ;
+    }
+
+    private String toDBType(final Column column) {
+        switch (column.getType()) {
+        case STRING:
+            // https://docs.microsoft.com/fr-fr/sql/relational-databases/tables/primary-and-foreign-key-constraints?view=sql-server-2017
+            return column.getSize() <= -1 ? (column.isPrimaryKey() ? "VARCHAR(900)" : "VARCHAR(8000)")
+                    : "VARCHAR(" + column.getSize() + ")";
+        case BOOLEAN:
+            return "BIT";
+        case DOUBLE:
+        case FLOAT:
+            return "DECIMAL";
+        case LONG:
+            return "BIGINT";
+        case INT:
+            return "INT";
+        case BYTES:
+            return "VARBINARY(max)";
+        case DATETIME:
+            return "datetime2";
+        case RECORD:
+        case ARRAY:
+        default:
+            throw new IllegalStateException(getI18n().errorUnsupportedType(column.getType().name(), column.getName()));
         }
     }
 }
