6efeb69af18d2a1a5e4693affa291acffeac83ec
undx <egallois@talend.com>
2019-10-07 12:20:02 +0200

    chore(TDI-42795): fix logging in exception blocks

--- azure-dls-gen2/src/main/java/org/talend/components/adlsgen2/runtime/input/ParquetBlobReader.java
+++ azure-dls-gen2/src/main/java/org/talend/components/adlsgen2/runtime/input/ParquetBlobReader.java
@@ -94,7 +94,7 @@ public class ParquetBlobReader extends BlobReader {
                 reader = AvroParquetReader.<GenericRecord> builder(hdpIn).build();
                 currentRecord = reader.read();
             } catch (IOException e) {
-                log.error("[ParquetIterator] {}", e);
+                log.error("[ParquetIterator] {}", e.getMessage());
                 throw new FileFormatRuntimeException(e.getMessage());
             }
         }
