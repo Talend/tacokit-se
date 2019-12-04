da47bc91f450407c49b2e7638949097e0761f107
Christophe Le Saec <clesaec@talend.com>
2019-12-04 17:51:03 +0100

    TDI-43099 : corrections first step

--- workday/src/main/java/org/talend/components/workday/package-info.java
+++ workday/src/main/java/org/talend/components/workday/package-info.java
@@ -1,8 +1,7 @@
 // this tells the framework in which family (group of components) and categories (UI grouping)
 // the components in the nested packages belong to
 @Components(family = "Workday", categories = { "Business", "Cloud" })
-// @Icon(value = Icon.IconType.CUSTOM, custom = "Workday")
-@Icon(value = Icon.IconType.BURGER)
+@Icon(value = Icon.IconType.CUSTOM, custom = "Workday")
 package org.talend.components.workday;
 
 import org.talend.sdk.component.api.component.Components;
