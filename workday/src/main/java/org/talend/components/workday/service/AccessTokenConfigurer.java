fdf02d0e18b52d648fbd8c6e28343735eb43a09c
Christophe Le Saec <clesaec@talend.com>
2020-05-07 08:36:07 +0200

    fix(TDI-44156) : don't follow redirect un-needed

--- workday/src/main/java/org/talend/components/workday/service/AccessTokenConfigurer.java
+++ workday/src/main/java/org/talend/components/workday/service/AccessTokenConfigurer.java
@@ -25,7 +25,6 @@ public class AccessTokenConfigurer implements Configurer {
     public void configure(Connection connection, ConfigurerConfiguration configuration) {
         log.debug("[configure] [{}] {}", connection.getMethod(), connection.getUrl());
         connection //
-                .withHeader("Content-Type", CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED) //
-                .withoutFollowRedirects();
+                .withHeader("Content-Type", CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED);
     }
 }
