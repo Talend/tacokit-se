52d9f94b508652b51a2653c12e0e55c2bf33915e
Oleksii Nimych <onimych@talend.com>
2020-12-01 12:39:16 +0200

    feat(TDI-44723): add truncate option

--- bigquery/src/main/java/org/talend/components/bigquery/output/BigQueryOutput.java
+++ bigquery/src/main/java/org/talend/components/bigquery/output/BigQueryOutput.java
@@ -13,12 +13,22 @@
 package org.talend.components.bigquery.output;
 
 import com.google.api.services.bigquery.model.TableReference;
+import com.google.cloud.WriteChannel;
 import com.google.cloud.bigquery.*;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import lombok.extern.slf4j.Slf4j;
 import org.talend.components.bigquery.datastore.BigQueryConnection;
 import org.talend.components.bigquery.service.BigQueryConnectorException;
 import org.talend.components.bigquery.service.BigQueryService;
+import org.talend.components.bigquery.service.GoogleStorageService;
 import org.talend.components.bigquery.service.I18nMessage;
+import org.talend.components.common.stream.api.RecordIORepository;
+import org.talend.components.common.stream.api.output.RecordWriter;
+import org.talend.components.common.stream.api.output.RecordWriterSupplier;
+import org.talend.components.common.stream.format.ContentFormat;
+import org.talend.components.common.stream.format.csv.CSVConfiguration;
 import org.talend.sdk.component.api.component.Icon;
 import org.talend.sdk.component.api.component.Version;
 import org.talend.sdk.component.api.configuration.Option;
@@ -31,10 +41,13 @@ import org.talend.sdk.component.api.record.Record;
 import org.talend.sdk.component.api.service.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.channels.Channels;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import static org.talend.sdk.component.api.component.Icon.IconType.BIGQUERY;
 
@@ -66,18 +79,84 @@ public class BigQueryOutput implements Serializable {
 
     private BigQueryService service;
 
+    private JobId jobId;
+
+    private GoogleStorageService storageService;
+
+    private transient Storage storage;
+
+    private RecordIORepository ioRepository;
+
+    private transient RecordWriter recordWriter;
+
+    private transient String blobName;
+
+    private transient BlobInfo blobInfo;
+
     public BigQueryOutput(@Option("configuration") final BigQueryOutputConfig configuration, BigQueryService bigQueryService,
-            I18nMessage i18n) {
+                          GoogleStorageService storageService, RecordIORepository ioRepository, I18nMessage i18n) {
         this.configuration = configuration;
         this.connection = configuration.getDataSet().getConnection();
         this.tableSchema = bigQueryService.guessSchema(configuration);
         this.service = bigQueryService;
+        this.jobId = getNewUniqueJobId();
+        this.storageService = storageService;
+        this.ioRepository = ioRepository;
         this.i18n = i18n;
+    }
+
+    private JobId getNewUniqueJobId() {
+        bigQuery = service.createClient(connection);
+        JobId jobId;
+        do {
+            jobId = JobId.of(UUID.randomUUID().toString());
+        } while (bigQuery.getJob(jobId) != null);
+        return jobId;
     }
 
     @PostConstruct
     public void init() {
+        if (BigQueryOutputConfig.TableOperation.TRUNCATE == configuration.getTableOperation()) {
+            bigQuery = service.createClient(connection);
+            storage = storageService.getStorage(bigQuery.getOptions().getCredentials());
+        }
+    }
 
+    @BeforeGroup
+    public void beforeGroup() {
+        records = new ArrayList<>();
+        if (BigQueryOutputConfig.TableOperation.TRUNCATE == configuration.getTableOperation()) {
+            long startTime = System.currentTimeMillis();
+            Blob blob = getNewBlob();
+            long endTime = System.currentTimeMillis() - startTime;
+            log.info("Create blob took " + endTime + " ms");
+            WriteChannel writer = blob.writer();
+            try {
+                long startTime1 = System.currentTimeMillis();
+                recordWriter = buildWriter(writer);
+                long endTime1 = System.currentTimeMillis() - startTime1;
+                log.info("Build csv writer took " + endTime1 + " ms");
+            } catch (IOException e) {
+                log.warn(e.getMessage());
+            }
+        }
+    }
+
+    private Blob getNewBlob() {
+        do {
+            String uuid = UUID.randomUUID().toString();
+            blobName = "temp/" + uuid + "/" + configuration.getDataSet().getTableName() + ".csv";
+            blobInfo = BlobInfo.newBuilder(configuration.getDataSet().getGsBucket(), blobName).build();
+        } while (storage.get(blobInfo.getBlobId()) != null);
+        return storage.create(blobInfo);
+    }
+
+    private RecordWriter buildWriter(WriteChannel writerChannel) throws IOException {
+        final ContentFormat contentFormat = new CSVConfiguration();
+        final RecordWriterSupplier recordWriterSupplier = this.ioRepository.findWriter(contentFormat.getClass());
+        final RecordWriter writer = recordWriterSupplier.getWriter(() -> Channels.newOutputStream(writerChannel), contentFormat);
+        writer.init(contentFormat);
+        return writer;
     }
 
     @ElementListener
@@ -102,11 +181,6 @@ public class BigQueryOutput implements Serializable {
         }
     }
 
-    @BeforeGroup
-    public void beforeGroup() {
-        records = new ArrayList<>();
-    }
-
     @AfterGroup
     public void afterGroup() {
 
@@ -115,19 +189,7 @@ public class BigQueryOutput implements Serializable {
             tableSchema = service.guessSchema(records.get(0));
 
             if (tableSchema != null) {
-                try {
-                    Table table = bigQuery.getTable(tableId);
-                    if (table == null) {
-                        log.info(i18n.infoTableNoExists(
-                                configuration.getDataSet().getBqDataset() + "." + configuration.getDataSet().getTableName()));
-                        TableInfo tableInfo = TableInfo.newBuilder(tableId, StandardTableDefinition.of(tableSchema)).build();
-                        table = bigQuery.create(tableInfo);
-                        log.info(i18n.infoTableCreated(tableId.getTable()));
-                    }
-                } catch (BigQueryException e) {
-                    log.warn(i18n.errorCreationTable() + e.getMessage(), e);
-
-                }
+                createTableIfNotExist();
             }
             if (tableSchema == null) {
                 // Retry reading table metadata, in case another worker created if meanwhile
@@ -138,6 +200,30 @@ public class BigQueryOutput implements Serializable {
             }
         }
 
+        if (BigQueryOutputConfig.TableOperation.TRUNCATE == configuration.getTableOperation()) {
+            loadData();
+        } else {
+            streamData();
+        }
+    }
+
+
+    private void createTableIfNotExist() {
+        try {
+            Table table = bigQuery.getTable(tableId);
+            if (table == null) {
+                log.info(i18n.infoTableNoExists(configuration.getDataSet().getBqDataset() + "." + tableId.getTable()));
+                TableInfo tableInfo = TableInfo.newBuilder(tableId, StandardTableDefinition.of(tableSchema)).build();
+                bigQuery.create(tableInfo);
+                log.info(i18n.infoTableCreated(tableId.getTable()));
+            }
+        } catch (BigQueryException e) {
+            log.warn(i18n.errorCreationTable() + e.getMessage(), e);
+
+        }
+    }
+
+    private void streamData() {
         TacoKitRecordToTableRowConverter converter = new TacoKitRecordToTableRowConverter(tableSchema, i18n);
 
         int nbRecordsToSend = records.size();
@@ -171,6 +257,57 @@ public class BigQueryOutput implements Serializable {
 
             nbRecordsSent += recordsBuffer.size();
         }
+    }
+
+    private void loadData() {
+        try {
+            long startTime = System.currentTimeMillis();
+            this.recordWriter.add(records);
+            this.recordWriter.end();
+            long endTime = System.currentTimeMillis() - startTime;
+            log.info("load data to the gs took " + endTime + " ms");
+        } catch (IOException exIO) {
+            log.error(exIO.getMessage());
+        }
+
+        String sourceUri = "gs://" + configuration.getDataSet().getGsBucket() + "/" + blobName;
+        Table table = bigQuery.getTable(tableId);
+        Schema schema = table.getDefinition().getSchema();
+        LoadJobConfiguration.Builder loadConfigurationBuilder = LoadJobConfiguration.newBuilder(tableId, sourceUri)
+                .setFormatOptions(FormatOptions.csv()).setSchema(schema);
+        Job firstJob = bigQuery.getJob(jobId);
+        JobInfo jobInfo;
+        if (firstJob == null) {
+            loadConfigurationBuilder.setWriteDisposition(JobInfo.WriteDisposition.WRITE_TRUNCATE);
+            jobInfo = JobInfo.newBuilder(loadConfigurationBuilder.build()).setJobId(jobId).build();
+        } else {
+            if (!firstJob.isDone()) {
+                try {
+                    firstJob.waitFor();
+                } catch (InterruptedException e) {
+                    log.warn(e.getMessage());
+                }
+            }
+            jobInfo = JobInfo.of(loadConfigurationBuilder.build());
+        }
+        long startTime = System.currentTimeMillis();
+        Job job = bigQuery.create(jobInfo);
+        try {
+            job = job.waitFor();
+            long endTime = System.currentTimeMillis() - startTime;
+            log.info("Load data from gs to bq took " + endTime + " ms");
+        } catch (InterruptedException e) {
+            log.warn(e.getMessage());
+        }
+        if (job.isDone()) {
+            log.info("CSV from GCS successfully loaded in a table");
+        } else {
+            log.warn("BigQuery was unable to load into the table due to an error:" + job.getStatus().getError());
+        }
+        long startTime3 = System.currentTimeMillis();
+        storage.delete(blobInfo.getBlobId());
+        long endTime3 = System.currentTimeMillis() - startTime3;
+        log.info("Delete blob took " + endTime3 + " ms");
     }
 
     private TableReference createTableReference() {
