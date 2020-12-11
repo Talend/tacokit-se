f92dd7e27c0608e89018e9a3c8ee1c1d43b35d36
Christophe Le Saec <clesaec@talend.com>
2020-12-11 12:06:27 +0100

    TDI-45317 : Remove test

--- common-stream-io/stream-json/src/test/java/org/talend/components/common/stream/output/json/RecordToJsonTest.java
+++ common-stream-io/stream-json/src/test/java/org/talend/components/common/stream/output/json/RecordToJsonTest.java
@@ -1,18 +1,9 @@
 package org.talend.components.common.stream.output.json;
 
-import java.util.Iterator;
-
 import javax.json.JsonObject;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.talend.components.common.test.records.DatasetGenerator;
-import org.talend.components.common.test.records.DatasetGenerator.DataSet;
-import org.talend.components.common.test.records.ExpectedValueBuilder;
-import org.talend.sdk.component.api.service.record.RecordBuilderFactory;
-import org.talend.sdk.component.runtime.record.RecordBuilderFactoryImpl;
 
 class RecordToJsonTest {
     final RecordToJson toJson = new RecordToJson();
@@ -23,19 +14,6 @@ class RecordToJsonTest {
         Assertions.assertNull(jsonObject);
     }
 
-    @ParameterizedTest
-    @MethodSource("testDataJson")
-    void testRecordsLine(DataSet<JsonObject> ds) {
-        final JsonObject jsonObject = toJson.fromRecord(ds.getRecord());
-        ds.check(jsonObject);
-    }
 
-
-    private static Iterator<DataSet<JsonObject>> testDataJson() {
-        final ExpectedValueBuilder<JsonObject> valueBuilder = new JsonExpected();
-        final RecordBuilderFactory factory = new RecordBuilderFactoryImpl("test");
-        DatasetGenerator<JsonObject> generator = new DatasetGenerator<>(factory, valueBuilder);
-        return generator.generate(40);
-    }
 
 }
